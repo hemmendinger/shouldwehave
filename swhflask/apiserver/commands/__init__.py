4de865ec194bc2bf6487b9ea597e4f6771a72ffe
Hemmendinger <github@hemmendinger.net>
2019-07-21 18:22:24 -0400

    rename functions

--- swhflask/apiserver/commands/__init__.py
+++ swhflask/apiserver/commands/__init__.py
@@ -1,23 +1,34 @@
 
 
 def init_app(app):
-    @app.cli.command('createdb')
-    def createdb_cli():
-        print('TODO create createdb')
+    @app.cli.command('create-db')
+    def create_db_cli():
+        # Is this an acceptable solution for getting db
+        from .. import db
+        db.create_all()
 
+    @app.cli.command('reset-db')
+    def reset_db_cli():
+        # Is this an acceptable solution for getting db
+        from .. import db
 
-    @app.cli.command('resetdb')
-    def resetdb_cli():
         ans = None
         while ans not in ("yes", "no"):
             ans = input("Warning: This will reset the entire database, destroying all data. Continue? yes/no > ")
             if ans == 'yes':
                 print('resets db')
+                db.drop_all()
                 return
             elif ans == 'no':
                 print('No changes.')
                 return
 
-        print('TODO create resetdb')
+    @app.cli.command('wipe-tables')
+    def wipe_tables_cli():
+        pass
+
+    @app.cli.command('add-dev-data')
+    def add_dev_data_cli():
+        from .. import db
 
 
