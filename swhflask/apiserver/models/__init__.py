d0b6c5003c7c17befbeead5c97e6126a76f58a7e
Hemmendinger <github@hemmendinger.net>
2019-07-30 17:06:57 -0400

    building out models, added marshmallow for model schemas

--- swhflask/apiserver/models/__init__.py
+++ swhflask/apiserver/models/__init__.py
@@ -1,12 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON as PG_JSON
+from flask_marshmallow import Marshmallow
+from marshmallow import Schema, fields, pre_load, validate
+
 
 db = SQLAlchemy()
-
-
-def init_app(app):
-    db.init_app(app)
+ma = Marshmallow()
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(254), unique=True, index=True)
+    screen_name = db.Column(db.String(64), index=True)
+
+
+class UserSchema(ma.Schema):
+    id = fields.Integer()
+
+
+class Group(db.Model):
+    __tablename__ = 'group'
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class GroupMenu(db.Model):
+    __tablename__ = 'group_menu'
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class MenuItem(db.Model):
+    __tablename__ = 'menu_item'
+    id = db.Column(db.Integer, primary_key=True)
+
+
