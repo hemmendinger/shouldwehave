53ee0ccd9578af1a3489d3bc2438f1effc08ec05
Hemmendinger <github@hemmendinger.net>
2019-09-02 15:35:55 -0400

    major restructuring in progress

--- swhreact/src/App.js
+++ swhreact/src/App.js
@@ -1,10 +1,48 @@
 import React from 'react';
 
+import {
+    BrowserRouter as Router,
+    Route,
+    NavLink,
+    Switch,
+} from 'react-router-dom';
+import About from './about';
+import Contact from './contact';
+import Home from './home';
+import RoundRobin from './roundrobin';
+import Users from './users'
+import NotFound from './statusCodes';
+
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
+                <li><NavLink activeClassName="active" to="/roundrobin">Try It Out</NavLink></li>
+                <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
+                <li><NavLink activeClassName="active" to="/users">Users</NavLink></li>
+                <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+            </ul>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/roundrobin" component={RoundRobin} />
+                <Route path="/users" component={Users} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/about" component={About} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+);
+
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Home</h1>
+            <div className="App">
+                        <header className="App-header">
+                            <h1 className="App-title">shouldwehave</h1>
+                        </header>
+                {routing}
             </div>
         )
     }
--- swhreact/src/index.js
+++ swhreact/src/index.js
@@ -2,44 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'typeface-roboto';
 import './index.css';
-import {
-    BrowserRouter as Router,
-    Route,
-    NavLink,
-    Switch,
-} from 'react-router-dom';
 import App from './App';
-import About from './about';
-import Contact from './contact';
-import RoundRobin from './roundrobin';
-import Users from './users'
-import NotFound from './statusCodes';
+
 import * as serviceWorker from './serviceWorker';
 
-const routing = (
-    <Router>
-        <div>
-            <ul>
-                <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
-                <li><NavLink activeClassName="active" to="/roundrobin">Try It Out</NavLink></li>
-                <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
-                <li><NavLink activeClassName="active" to="/users">Users</NavLink></li>
-                <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
-            </ul>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/roundrobin" component={RoundRobin} />
-                <Route path="/users" component={Users} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/about" component={About} />
-                <Route component={NotFound} />
-            </Switch>
-        </div>
-    </Router>
-);
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(App, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
