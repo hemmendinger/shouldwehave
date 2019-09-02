aee47e7c5d51f36d6966d4b3ae2d5be030254cfc
Hemmendinger <github@hemmendinger.net>
2019-09-02 15:43:11 -0400

    move const routing body inside App class return

--- swhreact/src/App.js
+++ swhreact/src/App.js
@@ -13,36 +13,32 @@ import RoundRobin from './roundrobin';
 import Users from './users'
 import NotFound from './statusCodes';
 
-const routing = (
-    <Router>
-        <div>
-            <ul>
-                <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
-                <li><NavLink activeClassName="active" to="/roundrobin">Try It Out</NavLink></li>
-                <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
-                <li><NavLink activeClassName="active" to="/users">Users</NavLink></li>
-                <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
-            </ul>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/roundrobin" component={RoundRobin} />
-                <Route path="/users" component={Users} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/about" component={About} />
-                <Route component={NotFound} />
-            </Switch>
-        </div>
-    </Router>
-);
-
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                        <header className="App-header">
-                            <h1 className="App-title">shouldwehave</h1>
-                        </header>
-                {routing}
+                <header className="App-header">
+                    <h1 className="App-title">shouldwehave</h1>
+                </header>
+                <Router>
+                    <div>
+                        <ul>
+                            <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
+                            <li><NavLink activeClassName="active" to="/roundrobin">Try It Out</NavLink></li>
+                            <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
+                            <li><NavLink activeClassName="active" to="/users">Users</NavLink></li>
+                            <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+                        </ul>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/roundrobin" component={RoundRobin} />
+                            <Route path="/users" component={Users} />
+                            <Route path="/contact" component={Contact} />
+                            <Route path="/about" component={About} />
+                            <Route component={NotFound} />
+                        </Switch>
+                    </div>
+                </Router>
             </div>
         )
     }
