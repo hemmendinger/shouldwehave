301d5a7c9500d13571c695dbc1d3b2b78fe8db00
Hemmendinger <github@hemmendinger.net>
2019-09-02 16:17:34 -0400

    add ThemeProvider

--- swhreact/src/App.js
+++ swhreact/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles'
+import { blue, indigo } from '@material-ui/core/colors'
+
 import {
     BrowserRouter as Router,
     Route,
     NavLink,
     Switch,
 } from 'react-router-dom';
+
 import About from './about';
 import Contact from './contact';
 import Home from './home';
@@ -13,32 +18,45 @@ import RoundRobin from './roundrobin';
 import Users from './users'
 import NotFound from './statusCodes';
 
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: blue[900]
+    },
+    primary: {
+      main: indigo[700]
+    }
+  },
+});
+
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">shouldwehave</h1>
-                </header>
-                <Router>
-                    <div>
-                        <ul>
-                            <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
-                            <li><NavLink activeClassName="active" to="/roundrobin">Try It Out</NavLink></li>
-                            <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
-                            <li><NavLink activeClassName="active" to="/users">Users</NavLink></li>
-                            <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
-                        </ul>
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route path="/roundrobin" component={RoundRobin} />
-                            <Route path="/users" component={Users} />
-                            <Route path="/contact" component={Contact} />
-                            <Route path="/about" component={About} />
-                            <Route component={NotFound} />
-                        </Switch>
-                    </div>
-                </Router>
+                <ThemeProvider theme={theme}>
+                    <header className="App-header">
+                        <h1 className="App-title">shouldwehave</h1>
+                    </header>
+                    <Router>
+                        <div>
+                            <ul>
+                                <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
+                                <li><NavLink activeClassName="active" to="/roundrobin">Try It Out</NavLink></li>
+                                <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
+                                <li><NavLink activeClassName="active" to="/users">Users</NavLink></li>
+                                <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+                            </ul>
+                            <Switch>
+                                <Route exact path="/" component={Home} />
+                                <Route path="/roundrobin" component={RoundRobin} />
+                                <Route path="/users" component={Users} />
+                                <Route path="/contact" component={Contact} />
+                                <Route path="/about" component={About} />
+                                <Route component={NotFound} />
+                            </Switch>
+                        </div>
+                    </Router>
+                </ThemeProvider>
             </div>
         )
     }
